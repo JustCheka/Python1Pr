d835bedf2a1c227415b478395100958e879a6552
JustCheka <cheka201@yandxe.ru>
2023-10-02 23:36:29 +0300

    H4

---
+++ HW4.py
@@ -0,0 +1,13 @@
+print("Введите a")
+a = int(input())
+
+print("Введите b")
+b = int(input())
+
+print("Введите c")
+c = int(input())
+
+if ((a - 1) * b == c or a == c or b == c):
+    print("yes")
+else:
+    print("no")
